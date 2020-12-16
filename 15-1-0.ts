ef08849acb0e9ae9d012341f077b704e5771a5bd
Asher Lantz <asherlantz@Asher.local>
2020-12-16 18:08:32 -0500

    re-added old day15

---
+++ 15-1-0.ts
@@ -0,0 +1,40 @@
+import { readLines } from "./deps.ts";
+
+async function main() {
+  let start: string[] = [];
+  for await (const line of readLines(Deno.stdin)) {
+    start = line.split(",");
+  }
+
+  let dict: { [num: string]: number } = {};
+
+  for (let i = 0; i < start.length; i++) {
+    dict[start[i]] = i;
+  }
+
+  // dict["0"] = start.length;
+  console.log(dict);
+  let lastSaid: string = "0";
+  // console.log(lastSaid);
+  let difference: number = 0;
+  for (let i = start.length; i < 30000000 - 1; i++) {
+    if (lastSaid in dict) {
+      // console.log("old");
+      difference = i - dict[lastSaid];
+      dict[lastSaid] = i;
+      lastSaid = difference.toString();
+    } else {
+      // console.log(dict);
+      // console.log(`${lastSaid} not in dict`);
+      dict[lastSaid] = i;
+      lastSaid = "0";
+    }
+
+    // console.log(lastSaid);
+  }
+
+  return lastSaid;
+}
+
+const output = await main();
+console.log(output);
